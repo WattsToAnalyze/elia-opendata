2563a51698bc579f5c849e16d93a2bf44202dfa2
WattsToAnalyze <contact.gargyagokhale@gmail.com>
2025-12-02 20:32:57 +0100

    fix some errors

--- elia_opendata/error.py
+++ elia_opendata/error.py
@@ -2,23 +2,19 @@
 Exception classes for the Elia OpenData API client.
 """
 
+
 class EliaError(Exception):
     """Base class for Elia OpenData API errors"""
     def __init__(self, message, response=None):
         super().__init__(message)
         self.response = response
 
+
 class RateLimitError(EliaError):
     """Raised when API rate limit is exceeded"""
-    def __init__(self, message, call_limit=None, reset_time=None, limit_time_unit=None, response=None):
+    def __init__(self, message, error_code="RateLimitError", response=None):
         super().__init__(message, response)
-        self.call_limit = call_limit
-        self.reset_time = reset_time
-        self.limit_time_unit = limit_time_unit
-
-class AuthError(EliaError):
-    """Raised when API authentication fails"""
-    pass
+        self.error_code = error_code
 
 class APIError(EliaError):
     """Raised when API returns an error response"""
@@ -26,13 +22,6 @@ class APIError(EliaError):
         super().__init__(message, response)
         self.error_code = error_code
 
-class ValidationError(EliaError):
-    """Raised when request parameters fail validation"""
-    pass
-
-class ConnectionError(EliaError):
-    """Raised when network connection fails"""
-    pass
 
 class ODSQLError(APIError):
     """Raised when ODSQL query is malformed"""
